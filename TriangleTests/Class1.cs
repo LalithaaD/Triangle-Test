90b1133adb53922c715ca38dab1df2ce88cb4db3
Lalitha Madhuri Divakaruni <lalithamadhuri3@gmail.com>
2024-03-06 20:33:45 -0500

    First Commit

---
+++ TriangleTests/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TriangleSolver;
+
+namespace TriangleTests
+    {
+    [TestFixture]
+    public class TriangleTests
+        {
+        // Valid Equilateral Triangle
+        // Only one (1) test for a valid equilateral triangle
+        [Test]
+        public void ValidEquilateralTriangle_Input5_OutputValidEquilateral()
+            {
+            // Arrange
+            int side = 5;
+            // Act
+            string result = Triangle.AnalyzeTriangle(side, side, side);
+            // Assert
+            Assert.AreEqual("Equilateral triangle", result);
+            }
+        
+    }
+
+
